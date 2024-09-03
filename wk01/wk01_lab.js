ef3b0b0b4f9a2e8aa55b243950f4f07b4edbbe81
aadi219 <aaadibadola@gmail.com>
2024-09-03 19:18:00 -0400

    wk01 lab exercise

---
+++ wk01/wk01_lab.js
@@ -0,0 +1,46 @@
+function capitalizeLetters(str) {
+  let words = str.split(" ");
+  words.forEach((word, index) => {
+    words[index] = word[0].toUpperCase() + word.substring(1);
+  });
+  return words.join(" ");
+}
+console.log("Exercise 1:");
+console.log(capitalizeLetters("the quick brown fox"));
+
+function max(a, b, c) {
+  if (a >= b && a >= c) return a;
+  else if (b >= a && b >= c) return b;
+  else if (c >= a && c >= b) return c;
+}
+console.log("Exercise 2:");
+console.log(max(1, 0, 1));
+console.log(max(0, -10, -20));
+console.log(max(1000, 510, 440));
+
+function right(str) {
+  if (str.length >= 3) {
+    strLen = str.length;
+    let last3 = str.substring(strLen - 3, strLen);
+    return last3 + str.substring(0, strLen - 3);
+  } else {
+    return str;
+  }
+}
+console.log("Exercise 3:");
+console.log(right("Python"));
+console.log(right("JavaScript"));
+console.log(right("Hi"));
+
+function angle_Type(angle) {
+  angle = angle % 360;
+  if (angle > 0 && angle < 90) return "Acute angle";
+  else if (angle == 90) return "Right angle";
+  else if (angle > 90 && angle < 180) return "Obtuse angle";
+  else if (angle == 180) return "Straight angle";
+}
+console.log("Exercise 4:");
+console.log(angle_Type(47));
+console.log(angle_Type(90));
+console.log(angle_Type(145));
+console.log(angle_Type(180));
